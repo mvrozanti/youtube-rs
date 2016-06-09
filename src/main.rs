a8a4d9a575ccee539cda4245778b2330ac6ee818
matthunz <matthunz2@gmail.com>
2016-06-08 20:24:13 -0400

    Add function to get url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate hyper;
+extern crate rustty;
+extern crate rustc_serialize;
+
+use hyper::client::{Client, Response};
+
+fn send_get(url: &str) -> Response {
+    let client = Client::new();
+    client.get(url).send().expect("Error loading url")
+}
+
 fn main() {
-    println!("Hello, world!");
+    send_get("http://youtube.com");
 }
